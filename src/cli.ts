5c47c6085e34184578f413c9233fb3c9d0cf850f
Lasana Murray <lmurray@quenk.com>
2017-09-11 16:54:11 -0400

    fix: add process type

--- src/cli.ts
+++ src/cli.ts
@@ -8,19 +8,19 @@ import * as fluture from 'fluture';
 import * as rcl from '@quenk/rcl';
 import * as jcon from '@quenk/jcon';
 import { resolve } from 'path';
-import { Either} from 'afpl';
+import { Either } from 'afpl';
 
 /**
  * CompileError
  */
-export function CompileError(path:string, e: Error) {
+export function CompileError(path: string, e: Error) {
 
-  this.message = `Error while processing ${path}:${os.EOL}${e.stack?e.stack:e.message}`;
-  this.stack = (new Error(this.message)).stack;
-  this.name = this.constructor.name;
+    this.message = `Error while processing ${path}:${os.EOL}${e.stack ? e.stack : e.message}`;
+    this.stack = (new Error(this.message)).stack;
+    this.name = this.constructor.name;
 
-  if (Error.hasOwnProperty('captureStackTrace'))
-    Error.captureStackTrace(this, this.constructor);
+    if (Error.hasOwnProperty('captureStackTrace'))
+        Error.captureStackTrace(this, this.constructor);
 
 }
 
@@ -29,12 +29,12 @@ CompileError.prototype.constructor = CompileError;
 
 export default CompileError
 
-const expand = (path:string, parent: string) => resolve(parent, path);
+const expand = (path: string, parent: string) => resolve(parent, path);
 
 interface Arguments {
 
-  '--modules': boolean
-  '<module>': string
+    '--modules': boolean
+    '<module>': string
 
 }
 
@@ -51,98 +51,98 @@ Options:
         version: require('../package.json').version
     });
 
-const stat = (path:string):fluture.Future<never, fs.Stats> => 
-  fluture.node<never, fs.Stats>(cb => fs.stat(path, cb));
+const stat = (path: string): fluture.Future<never, fs.Stats> =>
+    fluture.node<never, fs.Stats>(cb => fs.stat(path, cb));
 
-const isDirectory = (path:string) : fluture.Future<never, Either<boolean, boolean>>=> 
-  stat(path).chain(s=>fluture.of(Either.fromBoolean(s.isDirectory())));
+const isDirectory = (path: string): fluture.Future<never, Either<boolean, boolean>> =>
+    stat(path).chain(s => fluture.of(Either.fromBoolean(s.isDirectory())));
 
-const isFile = (path:string) : fluture.Future<never, Either<boolean, boolean>>=> 
-  stat(path).chain(s=>fluture.of(Either.fromBoolean(s.isFile())));
+const isFile = (path: string): fluture.Future<never, Either<boolean, boolean>> =>
+    stat(path).chain(s => fluture.of(Either.fromBoolean(s.isFile())));
 
-const readdir = (path:string) : fluture.Future<never, string[]> =>
-  fluture.node<never, string[]>(cb => fs.readdir(path, cb));
+const readdir = (path: string): fluture.Future<never, string[]> =>
+    fluture.node<never, string[]>(cb => fs.readdir(path, cb));
 
-const readFile = (path:string): fluture.Future<never, string> =>
-  fluture.node<never, string>(cb=>fs.readFile(path, 'utf8', cb))
+const readFile = (path: string): fluture.Future<never, string> =>
+    fluture.node<never, string>(cb => fs.readFile(path, 'utf8', cb))
 
-const writeFile = (path:string, contents:string): fluture.Future<never, void>=> 
-  fluture.node<never, void>(cb=>fs.writeFile(path, contents, cb));
+const writeFile = (path: string, contents: string): fluture.Future<never, void> =>
+    fluture.node<never, void>(cb => fs.writeFile(path, contents, cb));
 
 interface Compiler {
 
-  (s:string): string
+    (s: string): string
 
 }
 
-const compile = (contents:string, f:Compiler ) : fluture.Future<never, string>=> 
-  fluture.attempt<never, string>(()=>f(contents));
+const compile = (contents: string, f: Compiler): fluture.Future<never, string> =>
+    fluture.attempt<never, string>(() => f(contents));
 
-const compileError = (path:string, e:Error) => 
-  fluture.reject(new CompileError(path, e));
+const compileError = (path: string, e: Error) =>
+    fluture.reject(new CompileError(path, e));
 
 const empty = () => fluture.of('');
 
-const routeFile = (path:string) => 
-  stat(path)
-  .chain(()=>readFile(path))
-  .chainRej(empty)
-  .chain(contents=>compile(contents, rcl.compile))
-  .chainRej(e=>compileError(path, e))
-  
-const confFile = (path:string) => (routes:string) =>
-  stat(path)
-  .chain(()=>readFile(path))
-  .chainRej(empty)
-  .chain(contents=>compile(contents, jcon.compile))
-  .chainRej(e=>compileError(path, e))
-  .map(conf => 
+const routeFile = (path: string) =>
+    stat(path)
+        .chain(() => readFile(path))
+        .chainRej(empty)
+        .chain(contents => compile(contents, rcl.compile))
+        .chainRej(e => compileError(path, e))
 
-    `import * as tendril from '@quenk/tendril';${os.EOL}`+
-    `import * as express from 'express';${os.EOL}`+
-    `${routes}${os.EOL}${os.EOL}`+
-    `export const conf = (process)=>(${conf}) ${os.EOL}` +
-    `${os.EOL}`+
-    `export default (name:string)=>`+
-    `new tendril.app.Module(name, __dirname, conf(process), routes)`
-  
-  );
+const confFile = (path: string) => (routes: string) =>
+    stat(path)
+        .chain(() => readFile(path))
+        .chainRej(empty)
+        .chain(contents => compile(contents, jcon.compile))
+        .chainRej(e => compileError(path, e))
+        .map(conf =>
 
-const compileModule = (path:string) =>  routeFile(`${path}/routes`).chain(confFile(`${path}/conf`));
+            `import * as tendril from '@quenk/tendril';${os.EOL}` +
+            `import * as express from 'express';${os.EOL}` +
+            `${routes}${os.EOL}${os.EOL}` +
+            `export const conf = (process:Process)=>(${conf}) ${os.EOL}` +
+            `${os.EOL}` +
+            `export default (name:string)=>` +
+            `new tendril.app.Module(name, __dirname, conf(process), routes)`
 
-const compileApp = (path:string)=> 
-  compileModule(path)
-  .map(txt=> `${txt}`);
+        );
 
-const printError = (e:Error) => console.error(e.stack?e.stack:e);
+const compileModule = (path: string) => routeFile(`${path}/routes`).chain(confFile(`${path}/conf`));
 
-const noop = ()=>{}
+const compileApp = (path: string) =>
+    compileModule(path)
+        .map(txt => `${txt}`);
 
-const execute = (path:string) : fluture.Future<never, void>=> 
+const printError = (e: Error) => console.error(e.stack ? e.stack : e);
+
+const noop = () => { }
+
+const execute = (path: string): fluture.Future<never, void> =>
     Either
-   .fromBoolean(args['--modules'])
-   .map(()=>  
-       isDirectory(path)
-       .chain(e=>
-                e.cata(
-                  ()=>fluture.of(undefined),
-                  ()=> readdir(path)
-                        .chain(list=>
-                              fluture
-                                .parallel(1, list.map(p => execute(expand(p, path))))
-                                .chain(()=> compileModule(path))
-                                .chain((txt:string)=> writeFile(`${path}/index.ts`, txt))))))
-   .orRight(()=>
-       compileApp(path)
-      .chain((txt:string)=>writeFile(`${path}/index.ts`, txt))
-      .chain(()=> writeFile(`${path}/start.ts`,
-        `import 'source-map-support/register';${os.EOL}`+
-        `import * as tendril from '@quenk/tendril';${os.EOL}`+
-        `import createMain from './';${os.EOL}${os.EOL}`+
-        `let app = new tendril.app.Application(createMain('/'));${os.EOL}`+
-        `app.start();`
-      )))
-   .takeRight()
- 
+        .fromBoolean(args['--modules'])
+        .map(() =>
+            isDirectory(path)
+                .chain(e =>
+                    e.cata(
+                        () => fluture.of(undefined),
+                        () => readdir(path)
+                            .chain(list =>
+                                fluture
+                                    .parallel(1, list.map(p => execute(expand(p, path))))
+                                    .chain(() => compileModule(path))
+                                    .chain((txt: string) => writeFile(`${path}/index.ts`, txt))))))
+        .orRight(() =>
+            compileApp(path)
+                .chain((txt: string) => writeFile(`${path}/index.ts`, txt))
+                .chain(() => writeFile(`${path}/start.ts`,
+                    `import 'source-map-support/register';${os.EOL}` +
+                    `import * as tendril from '@quenk/tendril';${os.EOL}` +
+                    `import createMain from './';${os.EOL}${os.EOL}` +
+                    `let app = new tendril.app.Application(createMain('/'));${os.EOL}` +
+                    `app.start();`
+                )))
+        .takeRight()
+
 execute(expand(args['<module>'], process.cwd())).fork(printError, noop)
 
